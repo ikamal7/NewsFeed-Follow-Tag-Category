20e95aaee78e90c4ea86d91292757f87ecac1ffe
Kamal <kamal@Kamaml-MacBook-Pro>
2020-05-04 02:20:01 +0600

    version 1.0.0

--- assets/js/nfc_scripts.js
+++ assets/js/nfc_scripts.js
@@ -2,23 +2,23 @@
 
     //alert(nfc_data.ajax_url);
 
-    $(document).ready(function ($) {
+    $(document).ready(function () {
         $('.nfc-category-list li a').on('click', function (e) {
             let catid = $(this).data('cat-id');
+            
             $.ajax({
                 url:nfc_data.ajax_url,
                 type: 'post',
                 data:{ 
                     action: 'nfc_ajax_get_id',
-                    data: catid
+                    data: catid,
                 }
             }).done (function (response) {
-                if(response){
-                    $(this).text('unfollow');
-                    console.log(response);
-                }else{
-                    alert(response);
+                if(response == catid){
+                    $(".nfc-category-list li a[data-cat-id="+catid+"]").text('unfollow') 
                 }
+                
+                //console.log(response);
                
             });
             e.preventDefault();
--- includes/Admin/NFC_Widget.php
+++ includes/Admin/NFC_Widget.php
@@ -11,21 +11,75 @@ class NFC_Widget extends \WP_Widget {
 
         add_action( 'wp_ajax_nfc_ajax_get_id', [$this, 'nfc_ajax_get_id'] );
         add_action( 'wp_ajax_nopriv_nfc_ajax_get_id', [$this, 'nfc_ajax_get_id_no'] );
+        add_action( 'pre_get_posts', [$this, 'set_posts_cat'] );
+    }
+
+    /**
+     * @param $query
+     */
+    public function set_posts_cat( $q ) {
+        if ( is_user_logged_in() ) { // First check if we have a logged in user before doing anything
+            if ( $q->is_home() // Only targets the main page, home page
+                 && $q->is_main_query() // Only targets the main query
+            ) {
+                // Get the current logged in user
+                $current_logged_in_user = wp_get_current_user();
+                /*
+                 * We will now get the term/category object from the user display_name
+                 * You will need to make sure if this corresponds with your term/category
+                 * If not, use the correct info to match
+                 */
+                $term = get_user_meta( $current_logged_in_user->ID, 'post_cat_ids', true );
+
+                if ( $term ) { // Only filter the main query if we actually have a term with the desired name
+                    $q->set( 'cat', $term ); // Filter the posts to only show posts from the desired category
+                }
+            }
+        }
+
     }
 
     /**
      * @return mixed
      */
     public function nfc_ajax_get_id() {
-        if ( is_user_logged_in() ) {
-            $data         = isset( $_POST["data"] ) ? $_POST["data"] : '';
-            $cat_ids      = serialize( $data );
-            $current_user = wp_get_current_user();
-            update_user_meta( $current_user->ID, 'post_cat_ids', $cat_ids );
+        global $wpdb;
 
-            echo $data;
-            die();
+        if ( !isset( $_POST ) || empty( $_POST ) || !is_user_logged_in() ) {
+            echo 'Oops! Try again.';
+            exit;
         }
+        $current_user = wp_get_current_user();
+        $saved_ids    = get_user_meta( $current_user->ID, 'post_cat_ids', true );
+
+        $id_to_add = isset( $_POST["data"] ) ? $_POST["data"] : '';
+
+        if ( in_array( $id_to_add, $saved_ids ) ) {
+
+            if (  ( $key = array_search( $id_to_add, $saved_ids ) ) !== false ) {
+                unset( $saved_ids[$key] );
+                update_user_meta( $current_user->ID, 'post_cat_ids', $saved_ids );
+                echo 'remove from list';
+            }
+
+        } else {
+            //if list is empty, initialize it as an empty array
+            if ( $saved_ids == '' ) {
+                $saved_ids = array();
+            }
+
+            // push the new ID inside the array
+            array_push( $saved_ids, $id_to_add );
+
+            if ( update_user_meta( $current_user->ID, 'post_cat_ids', $saved_ids ) ) {
+                echo $id_to_add;
+            } else {
+                echo 'Failed: Could not update user meta.';
+            }
+        }
+
+        die();
+
     }
 
     public function nfc_ajax_get_id_no() {
@@ -40,23 +94,43 @@ class NFC_Widget extends \WP_Widget {
      * @param $instance
      */
     public function widget( $args, $instance ) {
-        //print_r();
+
         echo $args['before_widget'];
         if ( !empty( $instance['title'] ) ) {
             echo $args['before_title'] . apply_filters( 'widget_title', $instance['title'] ) . $args['after_title'];
         }
+        $current_logged_in_user = wp_get_current_user();
+        $user_followed_ids      = get_user_meta( $current_logged_in_user->ID, 'post_cat_ids', true );
 
         $categories = get_categories( array(
             'taxonomy'   => 'category',
             'orderby'    => 'name',
             'parent'     => 0,
             'hide_empty' => 0,
+            'exclude'    => $user_followed_ids,
         ) );
-        echo "<div>";
-        print_r( get_user_meta( wp_get_current_user()->ID, 'post_cat_ids', true ) );
-        echo "</div>";
+        $follow_cat_lists = get_categories( array(
+            'taxonomy'   => 'category',
+            'orderby'    => 'name',
+            'parent'     => 0,
+            'hide_empty' => 0,
+            'include'    => $user_followed_ids,
+        ) );
+
+        echo "<ul class='nfc-my-category-list'>";
+        //echo $follow_cat_lists;
+
+        echo "</ul>";
         echo "<ul class='nfc-category-list'>";
-        //$this->nfc_ajax_get_id();
+        if ( is_user_logged_in() ):
+            echo "<h2>My list</h2>";
+            foreach ( $follow_cat_lists as $follow_cat_list ):
+                printf( '<li>%s <a data-cat-id="%s" href="#" class="follow-cat">%s</a></li>', esc_html( $follow_cat_list->name ), esc_attr( $follow_cat_list->term_id ), __( 'Unfollow', 'nfc' ) );
+
+            endforeach;
+        endif;
+        echo "<h2>Category</h2>";
+
         foreach ( $categories as $category ):
             printf( '<li>%s <a data-cat-id="%s" href="#" class="follow-cat">%s</a></li>', esc_html( $category->name ), esc_attr( $category->term_id ), __( 'Follow', 'nfc' ) );
         endforeach;
